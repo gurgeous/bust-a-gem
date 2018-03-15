9ae2d16e47ff1cedcc4299e77a79447480306186
Adam Doppelt <amd@gurge.com>
2018-03-15 16:16:22 -0700

    checkpoint

--- src/etags.js
+++ src/etags.js
@@ -1,47 +1,50 @@
 const readline = require('readline');
 const fs = require('fs');
 
+// https://en.wikipedia.org/wiki/Ctags#Etags_2
 const [STATE_INIT, STATE_HEADER, STATE_TAG, STATE_ERROR] = [0, 1, 2, 3];
 const HEADER_RE = /^\x0c/;
-const TAG_RE = /^([^\x7f]+)\x7f([^\x01]+)\x01([^,]+),(.*)/;
+const TAG_RE = /^([^\x7f]+)\x7f([^\x01]+)\x01([^,]+)/;
 
-const etags = path => {
+const etags = file => {
   // this is what we return
-  const { mtime } = fs.statSync(path);
-  const etags = {
-    path,
+  const { mtime } = fs.statSync(file);
+  const results = {
+    file,
     mtime,
     tags: new Map(),
   };
-  const tags = etags.tags;
+  const tags = results.tags;
 
   // parse state
   let state = STATE_INIT;
   let header;
 
   return new Promise((resolve, reject) => {
-    const input = fs.createReadStream(path, { encoding: 'utf8' });
+    const input = fs.createReadStream(file, { encoding: 'utf8' });
     const lineReader = readline.createInterface({ input });
 
     const onLine = line => {
       switch (state) {
-        case STATE_INIT:
+        case STATE_INIT: {
           if (HEADER_RE.exec(line)) {
             state = STATE_HEADER;
           } else {
-            throw new Error(`${etags.path} isn't an etags file.`);
+            throw new Error(`${results.file} isn't an etags file.`);
           }
           break;
+        }
 
-        case STATE_HEADER:
+        case STATE_HEADER: {
           const split = line.split(',');
-          const path = split[0];
+          const file = split[0];
           const size = parseInt(split[1], 10);
-          header = { path, size };
+          header = { file, size };
           state = STATE_TAG;
           break;
+        }
 
-        case STATE_TAG:
+        case STATE_TAG: {
           // are we starting a new header?
           if (HEADER_RE.exec(line)) {
             state = STATE_HEADER;
@@ -55,19 +58,23 @@ const etags = path => {
           }
 
           // this is what we append
-          const tag = match[2];
+          const key = match[2];
           const lineno = parseInt(match[3], 10);
-          const element = [header.path, lineno];
+          const element = {
+            file: header.file,
+            line: lineno,
+          };
 
-          // append to etags
-          let a = tags.get(tag);
-          if (a === undefined) {
-            a = [];
-            tags.set(tag, a);
+          // append to results
+          let array = tags.get(key);
+          if (!array) {
+            array = [];
+            tags.set(key, array);
           }
-          a.push(element);
+          array.push(element);
 
           break;
+        }
       }
     };
 
@@ -83,7 +90,7 @@ const etags = path => {
       .on('close', () => {
         input.close();
         if (state != STATE_ERROR) {
-          resolve(etags);
+          resolve(results);
         }
       });
   });
--- src/goToDefinition.js
+++ src/goToDefinition.js
@@ -10,68 +10,59 @@ const vscode = require('vscode');
 // main command
 //
 
-const provideDefinition = (document, position, token) => {
-  //
-  // find tags
-  //
+const provideDefinition = (document, position) => {
+  const query = document.getText(document.getWordRangeAtPosition(position));
 
-  const root = util.rootDir();
-  if (!root) {
-    return;
-  }
-  const gemfile = util.findFileUp(root, 'Gemfile');
-  if (!gemfile) {
-    vscode.window.showErrorMessage(
-      'Bust-A-Gem: Go To Definition only works if you have a Gemfile.'
-    );
-    return;
-  }
+  return new Promise((resolve, reject) => {
+    //
+    // find tags
+    //
 
-  const tagsPath = path.join(path.dirname(gemfile), 'TAGS');
-  if (!fs.existsSync(tagsPath)) {
-    vscode.window.showErrorMessage(`Bust-A-Gem: ${tagsPath} does not exist.`);
-    return;
-  }
-
-  //
-  // load tags
-  //
-
-  etags(tagsPath).then(
-    etags => {
-      const elapsed = new Date().getTime() - tm;
-
-      const query = document.getText(document.getWordRangeAtPosition(position));
-      const locations = etags.tags.get(query);
-      if (locations) {
-        console.log(locations);
-      }
-      // return result
-    },
-    error => {
-      console.error(error);
-      vscode.window.showErrorMessage(`Bust-A-Gem: ${error.message}`);
+    const root = util.rootDir();
+    if (!root) {
+      reject();
     }
-  );
+    const gemfile = util.findFileUp(root, 'Gemfile');
+    if (!gemfile) {
+      vscode.window.showErrorMessage(
+        'Bust-A-Gem: Go To Definition only works if you have a Gemfile in your project.'
+      );
+      reject();
+    }
 
-  //
-  // go to definition
-  //
+    const tagsFile = path.join(path.dirname(gemfile), 'TAGS');
+    if (!fs.existsSync(tagsFile)) {
+      vscode.window.showErrorMessage(`Bust-A-Gem: ${tagsFile} does not exist.`);
+      reject();
+    }
 
-  console.log(query);
+    //
+    // load tags
+    //
 
-  // return locate.find(txt).then(matches => matches.map(locationConverter));
-  // const locationConverter = match => new vscode.Location(vscode.Uri.file(match.file), new vscode.Position(match.l
-  //   ine, match.char));
-  // load tags
-
-  // build tags for project
-  // build tags for gems (if set in config)
-  // load (or reload) tags
-  // now complete with tags data
+    etags(tagsFile).then(
+      etags => {
+        const tags = etags.tags.get(query);
+        const locations = tags.map(tag => {
+          console.log(tag);
+          const file = path.join(root, tag.file);
+          return new vscode.Location(vscode.Uri.file(file), new vscode.Position(tag.line - 1, 0));
+        });
+        resolve(locations);
+      },
+      error => {
+        vscode.window.showErrorMessage(`Bust-A-Gem: ${error.message}`);
+        reject();
+      }
+    );
+  });
 };
 
 const goToDefinition = { provideDefinition };
 module.exports = goToDefinition;
 
 // block ; time ripper-tags -f TAGS -R . /Users/amd/.rbenv/versions/2.5.0/lib/ruby/gems/2.5.0/gems/activerecord-5.1.5 /Users/amd/.rbenv/versions/2.5.0/lib/ruby/gems/2.5.0/gems/nokogiri-1.8.2
+
+// build tags for project
+// build tags for gems (if set in config)
+// load (or reload) tags
