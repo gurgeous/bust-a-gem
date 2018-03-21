e5e5e860aafdd03abe1a2c3816df10622b785b22
Adam Doppelt <amd@gurge.com>
2018-03-20 20:31:14 -0700

    some goTo tests

--- src/test/goTo.test.ts
+++ src/test/goTo.test.ts
@@ -1,16 +1,12 @@
 import { GoTo } from '../goTo';
 import * as assert from 'assert';
-import * as fs from 'fs-extra';
-import * as os from 'os';
+import * as child_process from 'child_process';
+import * as fs from 'fs';
 import * as path from 'path';
 import * as sinon from 'sinon';
-import * as testHelpers from './testHelpers';
-import * as util from '../util';
 import * as vscode from 'vscode';
 
-// provideDefinition will rip, then answer
 // provideDefinition picks up ::
-// provideDefinition won't rip twice
 //
 // guard: don't be reentrant (running)
 // guard: errors should be displayed in the window
@@ -18,41 +14,67 @@ import * as vscode from 'vscode';
 //
 // don't whine too often
 
-describe.only('Go To Definition', () => {
+describe('Go To Definition', () => {
   let sandbox: sinon.SinonSandbox;
-  let rootPath: string;
+  let document: vscode.TextDocument;
+  const rootPath = <string>vscode.workspace.rootPath;
+
+  //
+  // hooks
+  //
+
+  before(async () => {
+    // open something.rb as our document
+    const something = path.join(rootPath, 'something.rb');
+    document = await vscode.workspace.openTextDocument(something);
+  });
+
+  let exec: sinon.SinonStub;
+  let goTo: GoTo;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-
-    // create new workspace and make vscode think we are in there
-    rootPath = path.join(os.tmpdir(), 'bustagem_tests');
-    fs.removeSync(rootPath);
-    fs.mkdirpSync(rootPath);
-    sandbox.stub(vscode.workspace, 'rootPath').returns(rootPath);
-
-    // copy files into place
-    ['Gemfile', 'something.rb'].forEach(name => {
-      fs.copySync(testHelpers.fixtureFile(name), path.join(rootPath, name));
-    });
-    console.log('root is ' + rootPath);
+    // never exec
+    exec = sandbox.stub(child_process, 'exec').callsArgWith(2, null, 'stdout');
+    goTo = new GoTo();
   });
   afterEach(() => sandbox.restore());
 
-  it('works', async () => {
-    // open document
-    const something = path.join(rootPath, 'something.rb');
-    const document = await vscode.workspace.openTextDocument(something);
-    // // Create fake document. Saves 100ms vs calling openDocument.
-    // const document = <vscode.TextDocument>{
-    //   getText: () => testHelpers.readFixture('something.rb'),
-    //   uri: vscode.Uri.file('/ignore'),
-    // };
+  //
+  // tests
+  //
 
-    //
-    const goTo = new GoTo();
-    // const gub = await goTo.provideDefinition(document, new vscode.Position(2, 11));
-    const gub = await goTo.provideDefinition0('Hello');
-    console.log(gub);
+  const checkDefinition = async (callSite: RegExp, defSite: RegExp) => {
+    const lines = document.getText().split('\n');
+    const callLineIndex = lines.findIndex(i => i.match(callSite) !== null);
+    const defLineIndex = lines.findIndex(i => i.match(defSite) !== null);
+
+    // assume offset 9 at call site
+    // 01234567890
+    // ......p gub
+    const position = new vscode.Position(callLineIndex, 9);
+
+    const definitions = <vscode.Location[]>await goTo.provideDefinition(document, position);
+    assert.equal(definitions.length, 1);
+    assert.equal(definitions[0].range.start.line, defLineIndex);
+  };
+
+  it('provides definitions', async () => {
+    await checkDefinition(/^\s+p gub/, /^\s+def gub/);
+    await checkDefinition(/^\s+p Hello::World/, /^\s+class World/);
+  });
+
+  it('tries to rip', async () => {
+    // make TAGS not found
+    sandbox.stub(fs, 'existsSync').returns(false);
+
+    // go
+    await goTo.provideDefinition0('gub');
+
+    // did we try to rip?
+    assert.equal(exec.callCount, 1);
+    const args = exec.firstCall.args;
+    assert(args[0].startsWith('ripper-tags'));
+    assert.equal(args[1].cwd, rootPath);
   });
 });
