1ee6692154c6fdd4602b434f7906801a842aa6e8
Adam Doppelt <amd@gurge.com>
2020-04-19 16:52:47 -0700

    another stab at travis

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,23 +1,26 @@
 import * as path from 'path';
-
 import { runTests } from 'vscode-test';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    // amd
+    // Download VS Code, unzip it and run the integration test
+    // await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    const testWorkspace = path.resolve(__dirname, '../../src/test/fixtures');
+    const launchArgs = [testWorkspace];
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs });
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
 }
 
 main();
